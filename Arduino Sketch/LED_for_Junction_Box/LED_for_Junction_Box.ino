8a533b44e9a20868197f05b693717b18ba1ae9a9
openbuilds-engineer <p.walt@openbuilds.com>
2020-07-12 19:30:26 +0200

    update

--- Arduino Sketch/LED_for_Junction_Box/LED_for_Junction_Box.ino
+++ Arduino Sketch/LED_for_Junction_Box/LED_for_Junction_Box.ino
@@ -4,14 +4,14 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <WiFiClientSecure.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
 
 #include "index.h" //Our HTML webpage contents with javascripts
 
-
 #ifndef STASSID
-#define STASSID "BRAAI-FI"
+#define STASSID "30Bird-Stoep"
 #define STAPSK  "aabbccddeeff"
 #endif
 
@@ -25,10 +25,51 @@ const int redLED = 12;
 const int greenLED = 13; 
 const int blueLED = 14;  
 
+//#include <UniversalTelegramBot.h>
+//#define BOTtoken "1002866355:AAHytxbzXvwG8OQOusV5YTTtwMNcLVGUGko"  // your Bot Token (Get from Botfather)
+//WiFiClientSecure client;
+//UniversalTelegramBot bot(BOTtoken, client);
+//int botRequestDelay = 1000;
+//unsigned long lastTimeBotRan;
+
+
+#define pushButton 0
+#define onboardLED 2
+bool buttonPushed = false;
+
+#include <Ticker.h>
+Ticker ticker;
+void tick() {
+  //toggle state
+  digitalWrite(onboardLED, !digitalRead(onboardLED));     // set pin to the opposite state
+}
+
+#include <Wire.h>
+
 //===============================================================
 // This routines are executed when you open its IP in browser
 //===============================================================
 void handleRoot() {
+  // Serial.println("handle root..");
+  String red = httpServer.arg(0); // read RGB arguments
+  String green = httpServer.arg(1);
+  String blue = httpServer.arg(2);
+
+  if((red != "") && (green != "") && (blue != "")) {
+    analogWrite(redLED, 1023 - red.toInt());
+    analogWrite(greenLED, 1023 - green.toInt());
+    analogWrite(blueLED, 1023 - blue.toInt());
+  }
+
+
+  Serial.print("Red: ");
+  Serial.println(red.toInt()); 
+  Serial.print("Green: ");
+  Serial.println(green.toInt()); 
+  Serial.print("Blue: ");
+  Serial.println(blue.toInt()); 
+  Serial.println();
+
  String s = MAIN_page; //Read HTML contents
  httpServer.send(200, "text/html", s); //Send web page
 }
@@ -36,44 +77,84 @@ void handleRoot() {
 void handleADC() {
  int a = analogRead(A0);
  String adcValue = String(a);
- httpServer.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
+ httpServer.send(200, "text/plain", adcValue); //Send ADC value only to client ajax request
+}
+
+void handleD4() {
+ int d4 = digitalRead(4);
+ String d4Value = String(d4);
+ httpServer.send(200, "text/plain", d4Value); //Send ADC value only to client ajax request
+}
+
+void handleD5() {
+ int d5 = digitalRead(5);
+ String d5Value = String(d5); 
+ httpServer.send(200, "text/plain", d5Value); //Send ADC value only to client ajax request
+}
+
+void handlei2cscan() {
+
+  byte error, address;
+  int nDevices;
+  nDevices = 0;
+  String message = "Scanning: \n\n";
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0) {
+      message += "I2C device found at address 0x";
+      Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+      message += String(address, HEX);
+      message += "\n";
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      message+= "Unknown error at address 0x";
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+      message += String(address, HEX);
+      message += "\n";
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+    message += "No I2C devices found\n";
+  } else {
+    Serial.println("done\n");
+    message += "done\n";
+  }
+
+  httpServer.send(200, "text/plain", message); //Send ADC value only to client ajax request
 }
 
 void compileTime() {
  int a = analogRead(A0);
  String compileTime = String(F(__DATE__)) + String(" ") + String(F(__TIME__));
- httpServer.send(200, "text/plane", compileTime); //Send ADC value only to client ajax request
-}
-
-void rgbHandler() {
-  // Serial.println("handle root..");
-String red = httpServer.arg(0); // read RGB arguments
-String green = httpServer.arg(1);
-String blue = httpServer.arg(2);
-
-if((red != "") && (green != "") && (blue != ""))
-{
-  analogWrite(redLED, 1023 - red.toInt());
-  analogWrite(greenLED, 1023 - green.toInt());
-  analogWrite(blueLED, 1023 - blue.toInt());
+ httpServer.send(200, "text/plain", compileTime); //Send ADC value only to client ajax request
 }
 
 
-Serial.print("Red: ");
-Serial.println(red.toInt()); 
-Serial.print("Green: ");
-Serial.println(green.toInt()); 
-Serial.print("Blue: ");
-Serial.println(blue.toInt()); 
-Serial.println();
-
-httpServer.send(200, "text/html", rgbpage);
+ICACHE_RAM_ATTR void handleInterrupt() {
+  buttonPushed = true;
 }
-
-
 
 void setup(void) {
 
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  digitalWrite(redLED, HIGH);
+  digitalWrite(greenLED, HIGH);
+  digitalWrite(blueLED, HIGH);
+  
+  Wire.begin();
+ 
   Serial.begin(115200);
   Serial.println();
   Serial.println("Booting...");
@@ -81,7 +162,8 @@ void setup(void) {
   Serial.print (F(__DATE__)) ;
   Serial.print (" ") ;
   Serial.println (F(__TIME__)) ;
- 
+  
+  WiFi.hostname("Doorbell");
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
 
@@ -96,13 +178,73 @@ void setup(void) {
 
   httpServer.on("/", handleRoot);      //Which routine to handle at root location. This is display page
   httpServer.on("/readADC", handleADC); //This page is called by java Script AJAX
+  httpServer.on("/readD4", handleD4); //This page is called by java Script AJAX
+  httpServer.on("/readD5", handleD5); //This page is called by java Script AJAX
   httpServer.on("/compileTime", compileTime); //This page is called by java Script AJAX
-  httpServer.on("/rgb", rgbHandler); //This page is called by java Script AJAX
+  httpServer.on("/i2cscan", handlei2cscan);
   httpServer.begin();
 
   Serial.printf("HTTPUpdateServer ready!");
+
+//  client.setInsecure();
+
+  pinMode(pushButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pushButton), handleInterrupt, RISING);
+
+  pinMode(onboardLED, OUTPUT);
+  digitalWrite(onboardLED, HIGH);
 }
 
 void loop(void) {
+
+  if (buttonPushed) {
+    digitalWrite(onboardLED, LOW);
+    ticker.attach(0.2, tick);
+    Serial.println("Button pressed - sending telegram");
+//    String keyboardJson = "[[{ \"text\" : \"I'm coming\", \"callback_data\" : \"/coming\" }],[{ \"text\" : \"Come back Later\", \"callback_data\" : \"/later\" }],[{ \"text\" : \"ALARM\", \"callback_data\" : \"/alarm\" }]]";
+//    bot.sendMessageWithInlineKeyboard("@the30bird", "DOORBELL: Front door", "", keyboardJson);
+    buttonPushed = false;
+    ticker.detach();
+    digitalWrite(onboardLED, HIGH);
+  }
+
+//  if (millis() > lastTimeBotRan + botRequestDelay)  {
+//    
+//    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+//    while (numNewMessages) {
+//      Serial.println("got response");
+//      for (int i = 0; i < numNewMessages; i++) {
+//        if (bot.messages[i].type == "callback_query") {
+//          Serial.print("Callback button pressed by: ");
+//          Serial.println(bot.messages[i].from_id);
+//          Serial.print("Action: ");
+//          Serial.println(bot.messages[i].text);
+//          if (bot.messages[i].text == "/alarm") {
+//            digitalWrite(12, HIGH);
+//            delay(5000);
+//            digitalWrite(12, LOW);
+//          } else if (bot.messages[i].text == "/coming") {
+//            digitalWrite(13, HIGH);
+//            delay(5000);
+//            digitalWrite(13, LOW);
+//          } else if (bot.messages[i].text == "/later") {
+//            digitalWrite(14, HIGH);
+//            delay(5000);
+//            digitalWrite(14, LOW);
+//          }
+//          bot.sendMessage("@the30bird", bot.messages[i].text, "");
+//        } else {
+//          if (bot.messages[i].text == "/alarm") {
+//            digitalWrite(12, HIGH);
+//            delay(5000);
+//            digitalWrite(12, LOW);
+//          }
+//        }
+//      }
+//      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+//    }
+//
+//    lastTimeBotRan = millis();
+//  }
   httpServer.handleClient(); 
 }
--- Arduino Sketch/LED_for_Junction_Box/index.h
+++ Arduino Sketch/LED_for_Junction_Box/index.h
@@ -1,71 +1,4 @@
-String rgbpage = ""
-"<!DOCTYPE html><html><head><title>RGB control</title><meta name='mobile-web-app-capable' content='yes' />"
-"<meta name='viewport' content='width=device-width' /></head><body style='margin: 0px; padding: 0px;'>"
-"<canvas id='colorspace'></canvas></body>"
-"<script type='text/javascript'>"
-"(function () {"
-" var canvas = document.getElementById('colorspace');"
-" var ctx = canvas.getContext('2d');"
-" function drawCanvas() {"
-" var colours = ctx.createLinearGradient(0, 0, window.innerWidth, 0);"
-" for(var i=0; i <= 360; i+=10) {"
-" colours.addColorStop(i/360, 'hsl(' + i + ', 100%, 50%)');"
-" }"
-" ctx.fillStyle = colours;"
-" ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);"
-" var luminance = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);"
-" luminance.addColorStop(0, '#ffffff');"
-" luminance.addColorStop(0.05, '#ffffff');"
-" luminance.addColorStop(0.5, 'rgba(0,0,0,0)');"
-" luminance.addColorStop(0.95, '#000000');"
-" luminance.addColorStop(1, '#000000');"
-" ctx.fillStyle = luminance;"
-" ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);"
-" }"
-" var eventLocked = false;"
-" function handleEvent(clientX, clientY) {"
-" if(eventLocked) {"
-" return;"
-" }"
-" function colourCorrect(v) {"
-" return Math.round(1023-(v*v)/64);"
-" }"
-" var data = ctx.getImageData(clientX, clientY, 1, 1).data;"
-" var params = ["
-" 'r=' + colourCorrect(data[0]),"
-" 'g=' + colourCorrect(data[1]),"
-" 'b=' + colourCorrect(data[2])"
-" ].join('&');"
-" var req = new XMLHttpRequest();"
-" req.open('POST', '?' + params, true);"
-" req.send();"
-" eventLocked = true;"
-" req.onreadystatechange = function() {"
-" if(req.readyState == 4) {"
-" eventLocked = false;"
-" }"
-" }"
-" }"
-" canvas.addEventListener('click', function(event) {"
-" handleEvent(event.clientX, event.clientY, true);"
-" }, false);"
-" canvas.addEventListener('touchmove', function(event){"
-" handleEvent(event.touches[0].clientX, event.touches[0].clientY);"
-"}, false);"
-" function resizeCanvas() {"
-" canvas.width = window.innerWidth;"
-" canvas.height = window.innerHeight;"
-" drawCanvas();"
-" }"
-" window.addEventListener('resize', resizeCanvas, false);"
-" resizeCanvas();"
-" drawCanvas();"
-" document.ontouchmove = function(e) {e.preventDefault()};"
-" })();"
-"</script></html>";
-
 const char MAIN_page[] PROGMEM = R"=====(
-<!doctype html>
 <html>
 
 <head>
@@ -99,62 +32,180 @@ const char MAIN_page[] PROGMEM = R"=====(
       top: 0;
       opacity: 0;
     }
+
+    .red {
+      background-color: #880000;
+      color: #fff;
+    }
+
+    .tab {
+      padding: 10px;
+    }
   </style>
 </head>
 
 <body>
+
+  <div>
+    <button class="btn tablink red" onclick="openTab(event,'rgb')">RGB</button>
+    <button class="btn tablink" onclick="openTab(event,'channel')">Channel</button>
+    <button class="btn tablink" onclick="openTab(event,'update')">Firmware</button>
+  </div>
+
+  <div id="rgb" class="w3-container w3-border tab">
+    <canvas id='colorspace'></canvas>
+</body>
+</div>
+
+<div id="channel" class="w3-container w3-border tab" style="display:none">
+  <a href="/?r=0&g=1023&b=1023">RED (12)</a><br>
+  <a href="/?r=1023&g=0&b=1023">GREEN (13)</a><br>
+  <a href="/?r=1023&g=1023&b=0">BLUE (14)</a><br>
+  <a href="/i2cscan">I2C Scan</a><br>
+  <a href="/compileTime">CompileTime</a><br>
+  <a href="/readADC">ReadADC</a><br>
+  <a href="/readD4">ReadD4</a><br>
+  <a href="/readD5">ReadD5</a><br>
+  
+
+</div>
+
+<div id="update" class="w3-container w3-border tab" style="display:none">
   <div class="upload-btn-wrapper">
+    Select a new firmware .bin file to update the firmware on this device.
+    <hr>
     <button id="uploadBtn" class="btn">Firmware Update</button>
     <input type="file" name="firmware" id="firmware" accept=".bin,.bin.gz" />
   </div>
   <div id="uploadLog"></div>
+</div>
 
-  <script>
-    var countdown = 15 // seconds
-    function docReady(fn) {
-      // see if DOM is already available
-      if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-      } else {
-        document.addEventListener("DOMContentLoaded", fn);
+<script>
+  var countdown = 15 // seconds
+  function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" red", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " red";
+  }
+
+  function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
+  docReady(function() {
+    document.querySelector('#firmware').addEventListener('change', function(e) {
+      console.log(this, e)
+      document.getElementById("uploadBtn").disabled = true;
+      var file = this.files[0];
+      var fd = new FormData();
+      fd.append("firmware", file);
+      // These extra params aren't necessary but show that you can include other data.
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/update', true);
+      xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+          var percentComplete = parseInt((e.loaded / e.total) * 100);
+          console.log(percentComplete + '% uploaded');
+          document.getElementById('uploadBtn').innerHTML = 'Uploading: ' + percentComplete + '%';
+        }
+      };
+      xhr.onload = function() {
+        if (this.status == 200) {
+          console.log('Server got:', this.response);
+          setInterval(function() {
+            countdown = (countdown - 1);
+            document.getElementById('uploadBtn').innerHTML = 'Upload Complete';
+            document.getElementById('uploadLog').innerHTML = 'Rebooting in ' + countdown + ' seconds';
+            if (countdown == 0) {
+              window.location.href = "/";
+            }
+          }, 1000);
+        };
+      };
+      xhr.send(fd);
+    }, false);
+  });
+  (function() {
+    var canvas = document.getElementById('colorspace');
+    var ctx = canvas.getContext('2d');
+
+    function drawCanvas() {
+      var colours = ctx.createLinearGradient(0, 0, window.innerWidth, 0);
+      for (var i = 0; i <= 360; i += 10) {
+        colours.addColorStop(i / 360, 'hsl(' + i + ', 100%, 50%)');
+      }
+      ctx.fillStyle = colours;
+      ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+      var luminance = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
+      luminance.addColorStop(0, '#ffffff');
+      luminance.addColorStop(0.05, '#ffffff');
+      luminance.addColorStop(0.5, 'rgba(0,0,0,0)');
+      luminance.addColorStop(0.95, '#000000');
+      luminance.addColorStop(1, '#000000');
+      ctx.fillStyle = luminance;
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+    var eventLocked = false;
+
+    function handleEvent(clientX, clientY) {
+      if (eventLocked) {
+        return;
+      }
+
+      function colourCorrect(v) {
+        return Math.round(1023 - (v * v) / 64);
+      }
+      var data = ctx.getImageData(clientX, clientY, 1, 1).data;
+      var params = [
+        'r=' + colourCorrect(data[0]),
+        'g=' + colourCorrect(data[1]),
+        'b=' + colourCorrect(data[2])
+      ].join('&');
+      var req = new XMLHttpRequest();
+      req.open('POST', '?' + params, true);
+      req.send();
+      eventLocked = true;
+      req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+          eventLocked = false;
+        }
       }
     }
-    docReady(function() {
-      document.querySelector('#firmware').addEventListener('change', function(e) {
-        document.getElementById("uploadBtn").disabled = true;
-        var file = this.files[0];
-        var fd = new FormData();
-        fd.append("firmware", file);
-        // These extra params aren't necessary but show that you can include other data.
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/update', true);
-        xhr.upload.onprogress = function(e) {
-          if (e.lengthComputable) {
-            var percentComplete = parseInt((e.loaded / e.total) * 100);
-            console.log(percentComplete + '% uploaded');
-            document.getElementById('uploadBtn').innerHTML = 'Uploading: ' + percentComplete + '%';
-          }
-        };
-        xhr.onload = function() {
-          if (this.status == 200) {
-            console.log('Server got:', this.response);
-            setInterval(function() {
-              countdown = (countdown - 1);
-              document.getElementById('uploadBtn').innerHTML = 'Upload Complete';
-              document.getElementById('uploadLog').innerHTML = 'Rebooting in ' + countdown + ' seconds';
-              if (countdown == 0) {
-                window.location.href = "/";
-              }
-            }, 1000);
-          };
-        };
-        xhr.send(fd);
-      }, false);
-    });
-  </script>
+    canvas.addEventListener('click', function(event) {
+      handleEvent(event.clientX, event.clientY, true);
+    }, false);
+    canvas.addEventListener('touchmove', function(event) {
+      handleEvent(event.touches[0].clientX, event.touches[0].clientY);
+    }, false);
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      drawCanvas();
+    }
+    window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas();
+    drawCanvas();
+    document.ontouchmove = function(e) {
+      e.preventDefault()
+    };
+  })();
+</script>
 </body>
 
 </html>
-<html>
+
 )=====";
